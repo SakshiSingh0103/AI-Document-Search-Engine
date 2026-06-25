f08c4b8d89c5267232aef2017a375d9d902f3d1f
SakshiSingh0103 <sakshisingh01032003@gmail.com>
2026-06-25 13:28:54 +0530

    Completed AI Document Search Engine

--- indexer.py
+++ indexer.py
@@ -1,16 +1,48 @@
 import os
+from pypdf import PdfReader
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def build_index():
     documents = []
     filenames = []
 
-    for file in os.listdir("documents"):
-        with open("documents/" + file, "r", encoding="utf-8") as f:
-            documents.append(f.read())
-            filenames.append(file)
+    # Read TXT files
+    if os.path.exists("documents"):
+        for file in os.listdir("documents"):
+            if file.endswith(".txt"):
+                with open(
+                    os.path.join("documents", file),
+                    "r",
+                    encoding="utf-8"
+                ) as f:
+                    documents.append(f.read())
+                    filenames.append(file)
 
-    vectorizer = TfidfVectorizer(stop_words='english')
+    # Read PDF files
+    if os.path.exists("pdfs"):
+        for file in os.listdir("pdfs"):
+            if file.endswith(".pdf"):
+                pdf_text = ""
+
+                reader = PdfReader(
+                    os.path.join("pdfs", file)
+                )
+
+                for page in reader.pages:
+                    pdf_text += page.extract_text() or ""
+
+                documents.append(pdf_text)
+                filenames.append(file)
+
+    vectorizer = TfidfVectorizer(
+        stop_words="english"
+    )
+
     tfidf_matrix = vectorizer.fit_transform(documents)
 
-    return documents, filenames, vectorizer, tfidf_matrix
+    return (
+        documents,
+        filenames,
+        vectorizer,
+        tfidf_matrix
+    )
